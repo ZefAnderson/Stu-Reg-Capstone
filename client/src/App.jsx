f3736cb73d27f29db78c25242d6419731160438f
Mike Eldredge <maeldredge.pro@gmail.com>
2023-07-19 10:29:29 -0600

    make login button communicate with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { ReactDOM } from 'react-dom/client';
 // import { AdminPage } from './Pages/AdminPage';
 // import { CoursesPage }  from './Pages/CoursesPage';
 // import { RegistrationPage } from './Pages/RegistrationPage';
@@ -6,7 +7,8 @@ import { useState, useEffect } from 'react'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -14,14 +16,57 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('reached handleSubmit');
+    fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify({username: username, password: password}),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+//          setData(res.body);
+        } else {
+          const error = new Error(res.error);
+          throw error;
+        }
+
+      })
+      .catch(err => {
+        console.error(err);
+        alert(`Error logging in please try again ${err}`);
+      });
+  }
+
   return (
     <>
       <div>
       </div>
       <h1>Log in or create a new user</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username:
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>password
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type='submit'>Login</button>
+      </form >
       <button>
-          Create New User
+        Create New User
       </button>
       <div className="card">
         <p>
--- server/server.js
+++ server/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello there!" })
+})
+
+app.post('/api/login', (req, res) => {
+  console.log('/api/login endpoint reached');
+  console.log(req.body);
+  res.send('Hithere');
 })
 
 app.listen(PORT, () => {
